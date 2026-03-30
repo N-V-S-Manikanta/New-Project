ae08a71de38c5e15e3767c572ec78a20d7faa024
N V S Manikanta <manikanta.s@ncetmail.com>
2026-03-30 18:01:57 +0530

    Create src/index.js file with main entry point code

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// Main entry point for the application
+console.log('Welcome to New-Project!');
+
+// Example function
+export const greet = (name) => {
+  return `Hello, ${name}!`;
+};
+
+export default {
+  greet,
+};
